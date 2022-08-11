dcc5431e0fbf15f25581d0ad302fdd42951baa13
Maira <mairagonzalez2001@gmail.com>
2022-08-11 17:15:20 -0300

    Agregue el comando update

--- Pelucanina.sql
+++ Pelucanina.sql
@@ -8,5 +8,15 @@ INSERT INTO `Perro`
     NULL,
     'Leon',
     '2016-05-22',
-    'Masculino',
+    'Macho',
     '23426456')
+
+UPDATE `Perro`
+ SET `Fecha_nac` = '2021-11-20' 
+ WHERE `Perro`.
+ `ID_Perro` = 3
+
+ UPDATE `Perro`
+  SET `Fecha_nac` = '2011-02-23' 
+  WHERE `Perro`.
+  `ID_Perro` = 4
