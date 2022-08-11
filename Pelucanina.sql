264c20d6276561afa94719c822b2404e38a7f90b
Maira <mairagonzalez2001@gmail.com>
2022-08-11 18:16:36 -0300

    Cree las Foraneas key

--- Pelucanina.sql
+++ Pelucanina.sql
@@ -1,3 +1,76 @@
+/*---------------------------------------*/
+/*creacion base de datos */
+/*---------------------------------------*/
+CREATE DATABASE tsdwad_maira;
+
+/*---------------------------------------*/
+/*seleccion de la base de datos */
+/*---------------------------------------*/
+USE tsdwad_maira;
+
+/*---------------------------------------*/
+/*creacion de tabla Dueno*/
+/*---------------------------------------*/
+CREATE TABLE `tsdwad_maira`.`Dueno` 
+( `DNI` INT(8) NOT NULL ,
+ `Nombre` VARCHAR(10) NOT NULL ,
+  `Apellido` VARCHAR(20) NOT NULL ,
+   `Telefono` VARCHAR(10) NOT NULL ,
+    `Direccion` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`DNI`)
+    ) ENGINE = InnoDB;
+/*---------------------------------------*/
+/*consulta que lista todo los dueños cargados*/
+/*---------------------------------------*/
+SELECT * FROM Dueno;
+
+
+
+
+
+
+/*---------------------------------------*/
+/*creacion de tabla Perro */
+/*---------------------------------------*/
+CREATE TABLE `tsdwad_maira`.`Perro`
+ ( `ID_Perro` INT NOT NULL AUTO_INCREMENT ,
+  `Nombre` VARCHAR(65) NOT NULL ,
+   `Fecha_nac` DATE NOT NULL DEFAULT '2022-08-11' ,
+    `Sexo` VARCHAR(65) NOT NULL DEFAULT 'Hembra' ,
+     `DNI_dueno` INT(8) NOT NULL ,
+      PRIMARY KEY (`ID_Perro`),
+      FOREIGN KEY (`DNI_Dueno`) REFERENCES `Dueno` (`DNI`)
+      ) ENGINE = InnoDB;
+/*---------------------------------------*/
+/*consulta que lista todo los perros cargados*/
+/*---------------------------------------*/
+SELECT * FROM Perro;
+
+
+
+/*---------------------------------------*/
+/*creacion de tabla Historial*/
+/*---------------------------------------*/
+CREATE TABLE `tsdwad_maira`.`Historial`
+ ( `ID_Historial` INT NOT NULL AUTO_INCREMENT ,
+  `Fecha` DATE NOT NULL DEFAULT '2022-11-08' ,
+   `Perro` VARCHAR(65) NOT NULL ,
+    `Descripcion` VARCHAR(80) NOT NULL ,
+     `Monto` FLOAT(5) NOT NULL ,
+      PRIMARY KEY (`ID_Historial`), 
+      FOREIGN KEY (`Perro`) REFERENCES `Perro` (`ID_Perro`)
+      ) ENGINE = InnoDB;
+
+
+
+
+
+
+
+
+
+
+
 INSERT INTO `Perro` 
 (`ID_Perro`,
  `Nombre`,
@@ -6,10 +79,47 @@ INSERT INTO `Perro`
   `DNI_dueno`)
    VALUES (
     NULL,
-    'Leon',
+    'Pantera',
     '2016-05-22',
     'Macho',
     '23426456')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 UPDATE `Perro`
  SET `Fecha_nac` = '2021-11-20' 
